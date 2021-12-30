7c1168487b510f0c197d4ba0f3d894974ddb74e4
mecancode <mecancode@gmail.com>
2021-12-30 14:26:49 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let count1=0
+let count2=0
+let count3=0
+let count4=0
+let count5=0
+let totalCount=0
+
+const hex=[0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F]
+const color=document.querySelector(".color")
+
+const value1=document.querySelector("#value1")
+const value1=document.querySelector("#value1")
+const value1=document.querySelector("#value1")
+const value1=document.querySelector("#value1")
+const value1=document.querySelector("#value1")
+const totalValue=document.querySelector("#totalValue")
+
+const reset1=document.querySelector(".reset1")
+const reset1=document.querySelector(".reset1")
+const reset1=document.querySelector(".reset1")
+const reset1=document.querySelector(".reset1")
+const reset1=document.querySelector(".reset1")
+const resetTotal=document.querySelector(".resetTotal")
+
